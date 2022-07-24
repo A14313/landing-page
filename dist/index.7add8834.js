f9cc7cc5149da29d19dea4b03c14ec63e7512585
A14313 <95083999+A14313@users.noreply.github.com>
2022-07-24 16:23:26 +0800

    Updated the styles
    Created the style for navigation for mobile view

---
+++ dist/index.7add8834.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".mobile-nav-toggle"),t=document.querySelector(".primary-navigation");e.addEventListener("click",(()=>{t.hasAttribute("data-visible")?e.setAttribute("aria-expanded",!1):e.setAttribute("aria-expanded",!0),t.toggleAttribute("data-visible")}));
+//# sourceMappingURL=index.7add8834.js.map

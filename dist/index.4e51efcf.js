561966cfc7d6f8d27371e9092bb82202ba82b8d9
A14313 <95083999+A14313@users.noreply.github.com>
2022-07-29 16:34:08 +0800

    Updated general styling
    Updated the 2nd section, the font sizes, font colors, cta

---
+++ dist/index.4e51efcf.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".primary-header"),t=document.querySelector(".mobile-nav-toggle"),a=document.querySelector(".primary-navigation");t.addEventListener("click",(()=>{a.hasAttribute("data-visible")?t.setAttribute("aria-expanded",!1):t.setAttribute("aria-expanded",!0),a.toggleAttribute("data-visible"),e.toggleAttribute("data-overlay")}));
+//# sourceMappingURL=index.4e51efcf.js.map
--- dist/index.7add8834.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".mobile-nav-toggle"),t=document.querySelector(".primary-navigation");e.addEventListener("click",(()=>{t.hasAttribute("data-visible")?e.setAttribute("aria-expanded",!1):e.setAttribute("aria-expanded",!0),t.toggleAttribute("data-visible")}));
-//# sourceMappingURL=index.7add8834.js.map
